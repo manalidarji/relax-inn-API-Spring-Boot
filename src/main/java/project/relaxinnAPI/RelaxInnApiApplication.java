7a9070b757325d6d07108f63c4b74bec1c30b98f
Manali Darji <darjimanali93@gmail.com>
2022-07-15 17:06:22 -0400

    spring boot starter app

---
+++ src/main/java/project/relaxinnAPI/RelaxInnApiApplication.java
@@ -0,0 +1,15 @@
+package project.relaxinnAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelaxInnApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RelaxInnApiApplication.class, args);
+		
+		System.out.println("boo");
+	}
+
+}
---
+++ src/test/java/project/relaxinnAPI/RelaxInnApiApplicationTests.java
@@ -0,0 +1,13 @@
+package project.relaxinnAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelaxInnApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
