93d2cd6f8ac6948c1767ea5ffa239acbd155c939
Manali Darji <darjimanali93@gmail.com>
2022-07-16 18:52:35 -0400

    refactor: add 'limit' parameter for reading property types

--- src/main/java/project/relaxinnAPI/controller/PropertyTypeController.java
+++ src/main/java/project/relaxinnAPI/controller/PropertyTypeController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import project.relaxinnAPI.model.PropertyTypeModel;
@@ -17,8 +18,8 @@ public class PropertyTypeController {
 	PropertyTypeService propTypeServiceObj;
 	
 	@GetMapping("/propertyTypes")
-	public ResponseEntity<List<PropertyTypeModel>> getAllPropTypes(){
-		List<PropertyTypeModel> allPropTypes = propTypeServiceObj.getAllPropTypes();	
+	public ResponseEntity<List<PropertyTypeModel>> getAllPropTypes(@RequestParam(name = "limit", required = false) String limit){
+		List<PropertyTypeModel> allPropTypes = propTypeServiceObj.getAllPropTypes(limit);	
 		if(allPropTypes.isEmpty() || allPropTypes == null) {
 			return new ResponseEntity<List<PropertyTypeModel>>(allPropTypes, HttpStatus.NOT_FOUND);			
 		}
--- src/main/java/project/relaxinnAPI/service/PropertyTypeService.java
+++ src/main/java/project/relaxinnAPI/service/PropertyTypeService.java
@@ -1,5 +1,6 @@
 package project.relaxinnAPI.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,8 +14,29 @@ public class PropertyTypeService {
 	@Autowired
 	PropertyTypeDao propTypeDaoObj;
 	
-	public List<PropertyTypeModel> getAllPropTypes(){
-		return propTypeDaoObj.findAll();
+	public List<PropertyTypeModel> getAllPropTypes(String number){
+		List<PropertyTypeModel> allPropTypes = propTypeDaoObj.findAll();
+		
+		// return all prop types if number is null
+		if(number == null) {
+			return allPropTypes;
+		}
+		
+		// parse string into int
+		int limit = Integer.parseInt(number);
+		
+		// return all prop types if limit is less than or equal to 0
+		if(limit <= 0) {
+			return allPropTypes;
+		}
+				
+		List<PropertyTypeModel> limitedPropTypes = new ArrayList<PropertyTypeModel>();
+        for (PropertyTypeModel propType: allPropTypes) {
+        	if(limitedPropTypes.size() == limit) {
+        		break;
+        	}
+        	limitedPropTypes.add(propType);   
+        }		
+		return limitedPropTypes;
 	}
-
 }
