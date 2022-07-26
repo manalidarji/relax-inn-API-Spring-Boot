b0a809def21076e5f7eccaddca4d210ff698fe7f
Manali Darji <darjimanali93@gmail.com>
2022-07-26 18:27:24 -0400

    feat: add CORS support for front end part

---
+++ src/main/java/project/relaxinnAPI/security/CorsConfig.java
@@ -0,0 +1,18 @@
+package project.relaxinnAPI.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CorsConfig implements WebMvcConfigurer {
+	@Override 
+	public void addCorsMappings(CorsRegistry registry) { 
+		registry
+			.addMapping("/**")
+			.allowedOrigins("https://relax-inn-by-manali.netlify.app/");
+//			.allowedOrigins("http://localhost:3000/");		
+	} 
+}
